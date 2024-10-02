e4a3331dbb9bd7d5aed34b55f0413104cef21f79
julianneoterogilmer <julianneoterogilmer@localhost>
2024-10-02 15:20:40 -0500

    Completed Studio 4! :)

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,26 @@
 package studio4;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(Color.PINK);
+		StdDraw.filledRectangle(.5,.5,.4,.25);
+		StdDraw.setPenColor(Color.GREEN);
+		StdDraw.filledCircle(.2, .67, .05);
+		StdDraw.filledCircle(.8, .33, .05);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledSquare(.2, .33, .05);
+		StdDraw.filledSquare(.8, .67, .05);
+		StdDraw.setPenColor(160,32,240);
+		StdDraw.filledEllipse(.5, 0.5, 0.12, 0.07);
+		StdDraw.setPenColor(Color.WHITE);
+		Font font = new Font("Arial", Font.BOLD, 10);
+		StdDraw.setFont(font);
+		StdDraw.text(.5, 0.5, "Sienna, Cindy, Julianne");
 	}
+	
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,26 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y =  in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		if(shape.equals("rectangle")) {
+			if(filled == true)
+			{
+				StdDraw.setPenColor(red,green, blue);
+				StdDraw.filledRectangle(x,y,halfWidth,halfHeight);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			
+			}
+				
+		}
 	}
 }
